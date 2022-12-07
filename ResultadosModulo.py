34f279b18f594d8062a8361c075f340607672998
Hotcer <davisguarico27@gmail.com>
2022-12-07 07:03:10 -0500

    Resolucion

---
+++ ResultadosModulo.py
@@ -0,0 +1,7 @@
+import Operaciones as o
+
+sumaComputadoras = o.suma(25, 15)
+restaComputadoras = o.resta(25, 45)
+multiplicaComputadoras = o.multiplication(5, 4)
+dividiComputadoras = o.division(23, 5)
+print(sumaComputadoras, restaComputadoras, multiplicaComputadoras, dividiComputadoras)
